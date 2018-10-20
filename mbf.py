56e8938b31f3c8523ab62c011b52c33b05aa7e10
six49 <44326146+six49@users.noreply.github.com>
2018-10-21 02:38:47 +0700

    Update mbf.py

--- mbf.py
+++ mbf.py
@@ -341,7 +341,7 @@ def menu():
 # \rhBY\rp                                             PIRMANSX \rk#
 # \rhGroup FB\rp  https://m.facebook.com/groups/164201767529837 \rk#
 # \rhGitHub\rp                      https://github.com/pirmansx \rk#
-#     \rhGitHub\rp                  https://github.com/six49\rk#
+#     \rhGitHub\rp                  https://github.com/six49 \rk#
 ###########################################################''')
 	tampil('''\rk%s\n\rc1 \rhAmbil id dari group\n\rc2 \rhAmbil id dari daftar teman\n\rc3 \rmKELUAR\n\rk%s'''%('#'*20,'#'*20))
 	i = inputM('[?]PILIH',[1,2,3])
